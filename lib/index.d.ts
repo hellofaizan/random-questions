d55680e7b004397ddb9bf40e5636e42ab8ba1e74
Mohammad Faizan <faizancurious@gmail.com>
2024-11-07 00:09:45 +0530

    working

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,20 @@
+declare const noun: string[];
+declare const adjs: string[];
+declare const exclamations: string[];
+declare const firstNames: string[];
+declare const lastNames: string[];
+declare const questions: string[];
+
 declare function getQuestion(): string;
 declare function getPersonName(): string;
 
-export { getQuestion, getPersonName };
+export {
+  getQuestion,
+  getPersonName,
+  noun,
+  adjs,
+  exclamations,
+  firstNames,
+  lastNames,
+  questions,
+};
